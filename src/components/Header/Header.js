c2df591551d8ccaf559d6f335a865c21b3a52369
ES99BartSakowski <bsakowski@es99.agency>
2021-10-07 11:50:09 -0500

    first

---
+++ src/components/Header/Header.js
@@ -0,0 +1,30 @@
+import Link from "next/link";
+import { FaShoppingCart } from "react-icons/fa";
+
+import Container from "@components/Container";
+
+import styles from "./Header.module.scss";
+
+const Header = () => {
+  return (
+    <header className={styles.header}>
+      <Container className={styles.headerContainer}>
+        <p className={styles.headerTitle}>
+          <Link href='/'>
+            <a>Funtown Card Time</a>
+          </Link>
+        </p>
+        {/* <p className={styles.headerCart}>
+          <button>
+            <FaShoppingCart />
+            <span>
+              $0.00
+            </span>
+          </button>
+        </p> */}
+      </Container>
+    </header>
+  );
+};
+
+export default Header;
